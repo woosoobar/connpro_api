0557112b3ff076c219ccf88178940356949afb3f
woosoobar <woosoobar@gmail.com>
2024-10-16 23:16:35 +0900

    second commit

---
+++ src/main/java/org/example/connectproapi/ConnectproApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.connectproapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectproApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectproApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/connectproapi/ConnectproApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.connectproapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectproApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
